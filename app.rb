7abf9b200121468cf10f7503ba08853dff58cc04
BagelEnthusiast <nathan.richardson94@gmail.com>
2019-04-22 18:01:53 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 class Application < Sinatra::Base
   # Write your code here!
 
+  get '/' do 
+    erb :index
+  end
+
+  # post '/' do 
+  #   @user_name = 
+  #   erb :greet
+  # end
+
+  post '/greet' do
+    erb :greet
+  end
+
 end
